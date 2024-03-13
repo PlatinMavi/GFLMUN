e4d54528257e18d8df9ef4c4cc6dfad9a07faeaf
Alper Gezer <a.yz07x@gmail.com>
2024-03-13 22:17:15 +0300

    base

---
+++ src/pages/layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet, Link } from "react-router-dom";
+
+
+// BURAYI ELLEME BURA NAVBAR VE FOOTER
+
+const Layout = () => {
+    return (
+        <div>
+            
+            <Outlet />
+
+        </div>
+    )
+};
+
+export default Layout;
